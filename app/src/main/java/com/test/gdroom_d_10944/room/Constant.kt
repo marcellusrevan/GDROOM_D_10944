6e1aad5ad348ca41f49b4b263604f0cf6dfaeb10
marcellusrevan <marcellusrevan123@gmail.com>
2022-09-20 09:11:35 +0700

    guided

---
+++ app/src/main/java/com/test/gdroom_d_10944/room/Constant.kt
@@ -0,0 +1,9 @@
+package com.test.gdroom_d_10944.room
+
+class Constant {
+    companion object{
+        const val TYPE_READ = 0
+        const val TYPE_CREATE = 1
+        const val TYPE_UPDATE = 2
+    }
+}
---
+++ app/src/main/java/com/test/gdroom_d_10944/room/Note.kt
@@ -0,0 +1,12 @@
+package com.test.gdroom_d_10944.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
